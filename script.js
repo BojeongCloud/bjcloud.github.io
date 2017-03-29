33b5302b72caa739d34922a3240b41e1d196d6c4
Hepheir <hepheir@gmail.com>
2017-03-29 22:16:08 +0900

    material design

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var maxMobileWidth = 480;
+
 function detectMobile() {
-	if (window.innerWidth < 420) {
+	if (window.innerWidth < maxMobileWidth) {
 		document.getElementsByClassName('wrap')[0].classList.add('mobile');
 	} else {
 		document.getElementsByClassName('wrap')[0].classList.remove('mobile');
@@ -9,34 +11,3 @@ function detectMobile() {
 }
 detectMobile();
 window.addEventListener('resize', detectMobile);
-
-window.addEventListener('load', () => {
-	function createCORSRequest(method, url){
-		var xhr = new XMLHttpRequest();
-		if ("withCredentials" in xhr){
-			xhr.open(method, url, true);
-		} else if (typeof XDomainRequest != "undefined"){
-			xhr = new XDomainRequest();
-			xhr.open(method, url);
-		} else {
-			xhr = null;
-		}
-		return xhr;
-	}
-
-	var request = createCORSRequest("get", "https://raw.githubusercontent.com/BojeongCloud/bojeongcloud.github.io/master/partial/intro.html");
-	if (request){
-		request.onload = function(){
-			var newSection = document.createElement('section');
-			newSection.classList.add('loading');
-			request.send();
-
-			newSection.innerHTML = request.responseText;
-
-			document.body.appendChild(newSection);
-			setTimeout(() => {
-				newSection.classList.remove('loading');
-			}, 400);
-		};
-	}
-});
